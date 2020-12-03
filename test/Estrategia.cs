d4d45fa7c074a867b4fac1ccc8b4c46b78ae6386
VictorMBalbuena <victor.53aol@gmail.com>
2020-12-02 22:59:31 -0300

    consulta 2

--- test/Estrategia.cs
+++ test/Estrategia.cs
@@ -16,7 +16,30 @@ namespace DeepSpace
 
 		public String Consulta2( ArbolGeneral<Planeta> arbol)
 		{
-			return "Implementar";
+			Cola<ArbolGeneral<Planeta>> x=new Cola<ArbolGeneral<Planeta>>();
+			x.encolar(arbol);
+			int nivel=0;
+			string mensaje="Consulta2=";
+			while(!x.esVacia())
+			{
+				int elementos=x.cantElementos;
+				nivel++;
+				int cantidadPorNivel=0;
+				while(elementos-->0)
+				{
+					ArbolGeneral<Planeta> nodoActual=x.desencolar();
+					
+					if (nodoActual.getDatoRaiz().Poblacion()>10) {
+						cantidadPorNivel++;
+					}
+					foreach (ArbolGeneral<Planeta> hijo in nodoActual.getHijos()) 
+					{
+						x.encolar(hijo);
+					}
+				}
+				mensaje+="Nivel "+nivel+":"+cantidadPorNivel+"--";
+			}
+			return mensaje;
 		}
 
 
